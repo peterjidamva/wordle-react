df83e4a7bf5db9ce43e1e74ecb10352edad7e52a
peterjidamva <jidamvap@gmail.com>
2022-04-03 18:38:54 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        WORDLE CLONE 
+      </nav>
+      {/* <WordleBody/>
+      <Keyboard/> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/WordleBody.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function WordleBody() {
+  return (
+    <div>WordleBody</div>
+  )
+}
+
+export default WordleBody
